22b6fa6061178f208c8e4118dee7c5aeae34d306
anuroopharshini <pandiri.harshini@gmail.com>
2017-08-03 19:19:36 -0700

    small change

---
+++ Guru99Ant/src/ant2/Testngantexample.java
@@ -0,0 +1,15 @@
+package ant2;
+
+import java.util.Date;
+
+import org.testng.annotations.*;
+public class Testngantexample {				
+@Test
+	public void Guru99AntTestNGMethod(){					     
+		System.out.println("HELLO GURU99 ANT PROGRAM");					
+		System.out.println("TODAY's DATE IS->"+ currentDate() );					
+	}		
+	public static String currentDate(){					
+		return new Date().toString();					
+	}		
+}	
---
+++ Guru99Ant/src/ant2/testclass.java
@@ -0,0 +1,10 @@
+package ant2;
+import org.testng.annotations.*;
+
+public class testclass {
+	@Test
+	public void m1(){
+		
+		System.out.println("hello m1");
+	}
+}

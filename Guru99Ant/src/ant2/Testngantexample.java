7c9063c506ef0bd46778cb837dc7433b772bc18e
harshinianuroop <pandiri.harshini@gmail.com>
2017-08-31 20:30:22 -0700

    gitt

---
+++ Guru99Ant/src/ant2/Testngantexample.java
@@ -0,0 +1,16 @@
+package ant2;
+
+import java.util.Date;
+import org.testng.annotations.*;
+
+import org.testng.annotations.*;
+public class Testngantexample {				
+@Test
+	public void Guru99AntTestNGMethod(){					     
+		System.out.println("HELLO GURU99 ANT PROGRAM");					
+		System.out.println("TODAY's DATE IS->"+ currentDate() );					
+	}		
+	public static String currentDate(){					
+		return new Date().toString();					
+	}		
+}	

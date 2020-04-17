12554784497ef0a1fdbe97c3b38091c033808b6b
Honey <Honey@10.0.0.206>
2020-04-16 21:10:17 -0700

    testantexample a slight change of code

--- Guru99Ant/src/ant2/Testngantexample.java
+++ Guru99Ant/src/ant2/Testngantexample.java
@@ -7,7 +7,8 @@ import org.testng.annotations.*;
 public class Testngantexample {				
 @Test
 	public void Guru99AntTestNGMethod(){					     
-		System.out.println("HELLO GURU99 ANT PROGRAM");					
+		System.out.println("HELLO GURU99 ANT PROGRAM");		
+		System.out.println("HELLO honey");	
 		System.out.println("TODAY's DATE IS->"+ currentDate() );					
 	}		
 	public static String currentDate(){					
